915f6fe2875d43231e8c8ab9b23b6f0ae3676abb
moneym4st3r <101295961+moneym4st3r@users.noreply.github.com>
2022-05-27 14:43:32 +0300

    test

--- app.js
+++ app.js
@@ -1,55 +1,108 @@
 let tableBody = $('#tableBody');
-function getData(pairList){
+
+function getData(pairList) {
     //get all pair prices from binance api
     $.ajax({
         url: 'https://api.binance.com/api/v1/ticker/24hr',
         type: 'GET',
         dataType: 'json',
-        success: function(data){
-            render(data,pairList)
-        } 
+        success: function(data) {
+            console.log(data);
+            render(data, pairList)
+        }
     });
 
 }
 
-setInterval(function(){
+/* Global var for counter */
+var giCount = 1;
+
+$(document).ready(function() {
+	$('#example').dataTable();
+} );
+
+function fnClickAddRow(tableData) {
+    $('#example').dataTable().fnClearTable();
+    tableData.forEach(function(pair) {
+	$('#example').dataTable().fnAddData( [
+        pair.symbol,
+        pair.price,
+        pair.Dvolatility,
+        pair.stringofDigits,
+        pair.ppbp,
+        pair.volume,
+        pair.volpppbp,
+        pair.ppvol,
+        pair.ppvolpppbp,
+    ] );
+}
+);
+}
+setInterval(function() {
 
     pairListRaw = $('#pairList').val();
     pairList = pairListRaw.split(';');
     console.log(pairList);
     getData(pairList);
-},5000);
-function cleanFloat(num){
+}, 5000);
+
+function cleanFloat(num) {
     let sides = num.toString().split('.');
     let left = sides[0];
     let right = sides[1];
-    right.replaceAll('0','');
+    right.replaceAll('0', '');
     let newNum = left + '.' + right;
     return newNum;
 }
-function render(data,pairList){
+
+function render(data, pairList) {
     let html = "";
-    data.forEach(function(pair){
-        pairList.forEach(function(pairName){
-            if(pair.symbol == pairName.toUpperCase()){
+    let tabledata = [];
+    data.forEach(function(pair) {
+        pairList.forEach(function(pairName) {
+            if (pair.symbol == pairName.toUpperCase()) {
+                let stringofDigits = '';
                 let symbol = pair.symbol;
                 let price = pair.lastPrice;
+                let volume = pair.volume;
+                let Dvolatility = pair.priceChangePercent;
+                if(Dvolatility < 0){
+                    Dvolatility*= -1;
+                }
+                let vol = volume;
                 let leftright = parseFloat(price).toString().split('.');
-                let left = leftright[0];
-                let right = leftright[1];
-                let digitNo= right.length;
-                let stringofDigits = '0.';
-                for(let i = 0; i < digitNo-1; i++){
-                    stringofDigits += '0';
+                if (parseFloat(price).toString().includes('.')) {
+
+                    let left = leftright[0];
+                    let right = leftright[1];
+                    let digitNo = 0;
+                    if (right.length == null) {} else {
+                        digitNo = right.length;
+                    }
+                    stringofDigits += '0.';
+                    for (let i = 0; i < digitNo - 1; i++) {
+                        stringofDigits += '0';
+                    }
+
                 }
                 stringofDigits += '1';
-                let ppbp = ( parseFloat(price)-parseFloat(stringofDigits) )/parseFloat(stringofDigits);
-                html += '<tr><td>'+symbol+'</td><td>'+price+'</td><td>'+stringofDigits+'</td><td>'+parseInt(ppbp)+'</td></tr>';
-                
+                let ppbp = (parseFloat(price) - parseFloat(stringofDigits)) / parseFloat(stringofDigits);
+                let volpppbp = parseFloat(volume) / ppbp;
+                let ppvol =  parseFloat(volume)/parseFloat(price) ;
+                let ppvolpppbp = ppvol/ppbp;
+                html += '<tr><td>' + symbol + '</td><td>' + parseFloat(price) + '</td><td>' + parseFloat(stringofDigits) + '</td><td>' + parseInt(ppbp) + '</td><td>' + parseFloat(volume) + '</td><td>' + volpppbp + '</td><td>' + volpppbp + '</td><td>' + ppvolpppbp + '</td></tr>';
+                tabledata.push({ "symbol": symbol, "price": parseFloat(price),"Dvolatility":parseFloat( Dvolatility)+"%", "stringofDigits": parseFloat(stringofDigits), "ppbp": parseInt(ppbp), "volume": parseFloat(vol), "volpppbp": parseFloat(volpppbp).toFixed(2), "ppvol": parseFloat(ppvol).toFixed(2), "ppvolpppbp": parseFloat(ppvolpppbp).toFixed(2) });
                 console.log(ppbp)
             }
         });
-        $('#tableBodyID').html(html);
-});
-    ;
+    });
+
+    //populateDataTable(tabledata);
+    fnClickAddRow(tabledata)
 };
+
+
+
+$(document).ready(function() {
+    $("#example").DataTable();
+});
